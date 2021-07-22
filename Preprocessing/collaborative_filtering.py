247a97836b8a771be87c2dd86828abc4096a43d3
Sebi <mihaisebastian01@gmail.com>
2021-07-22 21:17:30 +0300

    format

--- Preprocessing/collaborative_filtering.py
+++
@@ -1,4 +0,0 @@
-"""The answer is Collaborative Filtering— by using the ratings dataset,
-we can identify similar users and movies, creating
-user and item embeddings learned from existing ratings."""
-
